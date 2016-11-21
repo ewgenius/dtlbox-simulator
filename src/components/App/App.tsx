c09f5d245805e1add79706ce3cb30b73f621a79a
ewgenius <ewgeniux@gmail.com>
2016-11-21 14:55:02 +0300

    ts config

---
+++ src/components/App/App.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react'
+import { Component } from 'react'
+import './App.css';
+
+export default class App extends Component<any, any> {
+  render() {
+    return <div className="App">
+      <div className="App-header">
+        <h2>Welcome to React</h2>
+      </div>
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+    </div>
+
+  }
+}
