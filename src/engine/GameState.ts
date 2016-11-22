0eb67c5e9df1ef9a04cf5a127b7e86299652aaf9
ewgenius <ewgeniux@gmail.com>
2016-11-22 17:42:16 +0300

    upd

--- src/engine/GameState.ts
+++ src/engine/GameState.ts
@@ -1,16 +1,31 @@
-import { Container } from 'pixi.js'
+import { Container, Sprite, loader } from 'pixi.js'
 import Player from './objects/Player'
+
+const DMITRUS = require('../../assets/dmitrus.png')
 
 export default class GameState extends Container {
   player: Player
-  
+
   constructor() {
     super()
   }
 
+  dmitrus: Sprite
+
   initState() {
+    loader
+      .add(DMITRUS)
+      .load(() => {
+        this.dmitrus = new Sprite(loader.resources[DMITRUS].texture)
+        //this.dmitrus.scale.x = 2
+        //this.dmitrus.scale.y = 2
+        this.addChild(this.dmitrus)
+      })
   }
 
   update() {
+    if (this.dmitrus) {
+      this.dmitrus.x += 0.1
+    }
   }
 }
