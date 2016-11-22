43359be3bc2310843ccb57d039b902f9e7f9d626
ewgenius <ewgeniux@gmail.com>
2016-11-22 11:06:41 +0300

    player

--- src/engine/GameState.ts
+++ src/engine/GameState.ts
@@ -1,6 +1,9 @@
 import { Container } from 'pixi.js'
+import Player from './objects/Player'
 
 export default class GameState extends Container {
+  player: Player
+  
   constructor() {
     super()
   }
---
+++ src/engine/objects/Player.ts
@@ -0,0 +1,22 @@
+import { Characteristic } from '../Characteristic'
+
+export default class Player {
+  private _productivity: Characteristic = 10
+  private _morality: Characteristic = 10
+
+  get productivity(): Characteristic {
+    return this._productivity
+  }
+
+  get morality(): Characteristic {
+    return this._morality
+  }
+
+  get name(): string {
+    return this._name
+  }
+
+  constructor(private _name: string) {
+
+  }
+}
